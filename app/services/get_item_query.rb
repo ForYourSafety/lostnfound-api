c40dbcaa24f19d4c1c7694244c52fef50dd7ca37
Sarah-Pan <sarah2950@gmail.com>
2025-06-04 21:22:49 +0800

    refactor: create get query class for item, account, and contact

---
+++ app/services/get_item_query.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module LostNFound
+  # get item query
+  class GetItemQuery
+    # Error for item
+    class ForbiddenError < StandardError
+      def message
+        'You are not allowed to edit or delete that item'
+      end
+    end
+
+    # Error for cannot find a item
+    class NotFoundError < StandardError
+      def message
+        'We could not find that item'
+      end
+    end
+
+    def self.call(account:, item_id:)
+      item = Item.first(id: item_id)
+      raise NotFoundError unless item
+
+      policy = ItemPolicy.new(account, item)
+      raise ForbiddenError unless policy.can_view?
+
+      item.full_details.merge(policies: policy.summary)
+    end
+  end
+end
