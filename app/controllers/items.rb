377458be225904ab6e0b0f0bdba5bc5039bf5d2d
Yi Kuo <kuokuoyiyi@gmail.com>
2025-06-08 19:45:52 +0800

    feat: include item info in item requests list

--- app/controllers/items.rb
+++ app/controllers/items.rb
@@ -69,7 +69,12 @@ module LostNFound
 
             requests = RequestPolicy::AccountItemScope.new(@auth, item).viewable
 
-            { data: requests }.to_json
+            response_data = {
+              item: item,
+              requests: requests
+            }
+
+            { data: response_data }.to_json
           rescue StandardError => e
             Api.logger.error "UNKNOWN ERROR: #{e.message}"
             routing.halt 500, { message: 'Unknown server error' }.to_json
