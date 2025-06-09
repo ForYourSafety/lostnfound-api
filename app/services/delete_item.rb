b7657705e081e0155c0f9f912494790699068c6b
Yi Kuo <kuokuoyiyi@gmail.com>
2025-06-09 21:13:30 +0800

    feat: delete images on item delete

--- app/services/delete_item.rb
+++ app/services/delete_item.rb
@@ -24,8 +24,20 @@ module LostNFound
       policy = ItemPolicy.new(auth, item)
       raise ForbiddenError unless policy.can_delete?
 
+      delete_images(item.image_keys.split(',')) if item.image_keys
+
       item.destroy
       item
+    end
+
+    def self.delete_images(image_keys)
+      return if image_keys.nil? || image_keys.empty?
+
+      image_keys.each do |image_key|
+        S3Storage.delete(object_key: image_key)
+      end
+    rescue StandardError => e
+      App.logger.warn("Failed to delete images: #{e.message}")
     end
   end
 end
